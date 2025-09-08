6fb061439f34aa7caf4090be3e0bc04b3a4841b9
imfromkoreah <xxivong@naver.com>
2025-09-08 19:22:30 +0900

    3-sol4

---
+++ 3-sol4/WebServer.py
@@ -0,0 +1,33 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from datetime import datetime
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # 200 OK 응답
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+
+        # index.html 파일 읽어서 전송
+        try:
+            with open('index.html', 'r', encoding='utf-8') as file:
+                html_content = file.read()
+                self.wfile.write(html_content.encode('utf-8'))
+        except FileNotFoundError:
+            self.wfile.write('<h1>index.html 파일이 없습니다.</h1>'.encode('utf-8'))
+
+        # 접속 정보 출력
+        client_ip = self.client_address[0]
+        access_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print('접속 시간: {}, 클라이언트 IP: {}'.format(access_time, client_ip))
+
+
+def run_server(host='0.0.0.0', port=8080):
+    server_address = (host, port)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print('웹 서버 시작: {}:{}'.format(host, port))
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    run_server()
